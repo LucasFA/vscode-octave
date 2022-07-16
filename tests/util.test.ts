a8779b59d06f0dae0f84cccb99a922bb10301ee9
LucasFA <lucaslanzarote@gmail.com>
2022-07-16 16:33:33 +0200

    Add testing capability

---
+++ tests/util.test.ts
@@ -0,0 +1,9 @@
+import * as extension from '../src/extension';
+import * as command from '../src/commands';
+import * as vscode from 'vscode'
+
+describe('(not) testing util.ts', () => {
+    test('Test description)', () => {
+        return;
+    });
+});
