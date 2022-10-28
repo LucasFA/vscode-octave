9509dc641b2a0e30c9d8a0dbdda2729d75afc927
LucasFA <lucaslanzarote@gmail.com>
2022-10-28 18:22:38 +0200

    No need for void here

--- src/util.ts
+++ src/util.ts
@@ -2,13 +2,13 @@ import * as vscode from "vscode";
 
 export async function saveDocument(document: vscode.TextDocument): Promise<boolean> {
     if (document.isUntitled) {
-        void vscode.window.showErrorMessage('Document is unsaved. Please save and retry.');
+        vscode.window.showErrorMessage('Document is unsaved. Please save and retry.');
         return false;
     }
 
     const isSaved: boolean = document.isDirty ? (await document.save()) : true;
     if (!isSaved) {
-        void vscode.window.showErrorMessage('Cannot run command. Document could not be saved.');
+        vscode.window.showErrorMessage('Cannot run command. Document could not be saved.');
     }
     return isSaved;
 }
